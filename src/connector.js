c925aae71c6a58b72f3b8302e58c34ee65b2e77a
Berikai <berkayeren2009@gmail.com>
2023-01-03 17:46:56 +0300

    Added comment lines to make code understandable

--- src/connector.js
+++ src/connector.js
@@ -1,12 +1,20 @@
+// MADE BY BERIKAI 2023
+// https://github.com/berikai
+
+// Middler Connector is responsible for creating virtual clients that represents real clients on host's machine.
+
 const net = require('net');
 const log = require('./libs/log');
 
+// Store connected clients
 let clients = [];
+// Create new Socket to connect Tunnel
 const tunnel = new net.Socket();
 
+// Middler Connector Config
 const config = {
 	ip: {
-		tunnel: '192.168.1.19',
+		tunnel: '192.168.1.8',
 		host: 'localhost'
 	},
 	port: {
@@ -15,26 +23,94 @@ const config = {
 	},
 };
 
-// VirtualClient Object
-const VirtualClient = function (id) {
-	this.id = id; // as hex form
-	this.socket = new net.Socket(); // create socket
+// VirtualClient represents the clients that are connected to the Virtual Hosts on Middler Tunnel by simulating them on host's machine.
+const VirtualClient = (id) => {
+	// CLIENT ID IN HEX FORM!
+	this.id = id;
+
+	// Create a new Socket to simulate real clients on host's machine.
+	this.socket = new net.Socket();
+
+	// Send data to host's server as client
 	this.write = data => this.socket.write(data);
+
+	// Connect to host's server
 	this.socket.connect(config.port.host, config.ip.host, () => log('Connected to host.', `VirtualClient${Buffer.from(this.id, 'hex').toString('ascii')}`));
+
+	// Send data to Middler Tunnel from host's server.
 	this.socket.on('data', data => tunnel.write(Buffer.from(this.id + data.toString('hex'), 'hex')));
+
+	// Log if error occurs
 	this.socket.on('error', err => log(err, `VirtualClient${Buffer.from(this.id, 'hex').toString('ascii')}`));
+
+	// If connection to host's server is closed, let the VirtualHost on Tunnel know
 	this.socket.on('close', () => {
+		// Remove the client from client array
 		clients = clients.filter(client => client.id != this.id);
 		log('Connection lost.', `VirtualClient${Buffer.from(this.id, 'hex').toString('ascii')}`)
 		tunnel.write(`${Buffer.from(this.id, 'hex')}CLIENT_LOST`);
 	});
 }
 
-// Tunnel Connector
+// Connect to the tunnel as Middler Connector
 tunnel.connect(config.port.tunnel, config.ip.tunnel, () => log('Connected to tunnel.', 'Node'));
+
+// Log if error occurs
 tunnel.on('error', (err) => log(err, 'Connector'));
+
+// Log if connection ends with Tunnel
 tunnel.on('close', () => log('Connection lost.', 'Connector'));
+
+// Process the data coming from Tunnel
 tunnel.on('data', (data) => {
+	// If Middler data prefixes exist
+	switch (true) {
+		// VirtualHost created
+		case (Buffer.from(data.toString('hex').substring(0, 18), 'hex') == 'HOST_PORT'):
+			log(`Virtual host created: ${Buffer.from(data.toString('hex').substring(18, data.toString('hex').length), 'hex')}`, 'Connector');
+			break;
+
+		// New client connected to VirtualHost
+		case (Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex') == 'NEW_CLIENT'):
+			// If client is not created before
+			if (clients.filter(client => client.id == data.toString('hex').substring(0, 24)) < 1) {
+				// Push new VirtualClient to client storage array
+				clients.push(new VirtualClient(data.toString('hex').substring(0, 24)));
+				log(`New virtual client created: ${Buffer.from(clients[clients.length - 1].id, 'hex').toString('ascii')}`, 'Connector');
+			} 
+			// Else, probably client already exists
+			else log(`ERROR: Could not create new client. Client already exists with id: ${Buffer.from(clients[clients.length - 1].id, 'hex').toString('ascii')}`, 'Connector');
+			break;
+
+		// Client connection lost on VirtualHost
+		case (Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex') == 'CLIENT_LOST'):
+			// Destroy the client
+			clients.map(client => {
+				if (client.id == data.toString('hex').substring(0, 24)) {
+					client.socket.destroy();
+				}
+			});
+	
+			// Remove the client from client storage array
+			clients = clients.filter(client => client.id != data.toString('hex').substring(0, 24));
+			break;
+
+		// If there is no Middler data prefixes
+		default:
+			clients.map(client => {
+				if (client.id == data.toString('hex').substring(0, 24)) {
+					// Send data to client from Tunnel
+					client.write(Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex'));
+				} else {
+					log('Could not find client.', 'Connector');
+				}
+			});
+
+			break;
+
+	}
+
+	/* Old ugly if else block
 	if (Buffer.from(data.toString('hex').substring(0, 18), 'hex') == 'HOST_PORT') {
 		log(`Virtual host created: ${Buffer.from(data.toString('hex').substring(18, data.toString('hex').length), 'hex')}`, 'Connector');
 	} else if (Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex') == 'NEW_CLIENT') {
@@ -60,4 +136,5 @@ tunnel.on('data', (data) => {
 			}
 		});
 	}
+	*/
 });
--- src/tunnel.js
+++ src/tunnel.js
@@ -1,10 +1,17 @@
+// MADE BY BERIKAI 2023
+// https://github.com/berikai
+
+// Middler Tunnel, is the main server of Middler. It's responsible for creating virtual hosts that represents real hosts. 
+// Tunnel creates a virtual host once a real host connects to the Tunnel with Middler Connector.
+
 const net = require('net');
 const log = require('./libs/log');
 const ascii_to_hex = require('./libs/ascii_to_hex');
 
+// Tunnel Config
 const config = {
     ip: {
-        tunnel: '192.168.1.19',
+        tunnel: '192.168.1.8',
         virtual_host: 'tcp.middler.io'
     },
     port: {
@@ -12,60 +19,106 @@ const config = {
     },
 };
 
-const HostManager = new function () {
+// HostManager manages the hosts that are connected to the Middler Tunnel with Middler Connector
+// NOTE: I wanted to achieve this "new function()" declaration with arrow functions but there is a potential that I do not understand what are arrow functions really is.
+const HostManager = new function() {
+    // Store connected hosts
     this.hosts = [];
-    this.createPort = () => { // MAKE IT BETTER <<<
+
+    // Provide port for new host
+    // THIS FUNCTION IS NOT USING A WELL APPROACH. MAKE IT BETTER!
+    this.providePort = () => {
         let port = Math.floor(Math.random() * 10000) + config.port.tunnel + 1;
+
         let samePorts = this.hosts.filter((host) => {
             return host.port == port;
         }).length;
+
         if (samePorts < 1) return port;
-        else this.createPort();
-    }; // <<<
+        else this.providePort();
+    };
+
+    // Push new host to hosts array
     this.pushHost = (host) => {
         this.hosts.push(host);
     };
+
+    // Remove the host from hosts array which is specified with its port number
     this.removeHost = (port) => {
         this.hosts.map((host, i) => {
             if (host.port == port) this.hosts.splice(i, 1);
         });
     };
+
 };
 
-const VirtualHost = function () {
+// VirtualHost represents the hosts that are connected to Middler Tunnel with Middler Connector
+// NOTE: Arrow function declaration didn't work. Probably arrow functions doesn't support OOP approach. I don't know what is the exact reason but that is the error output: "TypeError: VirtualHost is not a constructor"
+const VirtualHost = function() {
+    // Store callback functions
     this.callbacks = {
         data: [],
         write: [],
         client_close: [],
-    }; // callbacks will be stored here
-    this.port = HostManager.createPort();
+    };
+
+    // Get an available port from HostManager
+    this.port = HostManager.providePort();
+
+    // Send data to client from Middler Connector
     this.write = (data) => {
+        // If VirtualClient that is connected to real hosts on Middler Connector closed, close the real client that is connected to Virtual Host on Middler Tunnel
         if (Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex') == 'CLIENT_LOST') {
             this.callbacks['client_close'].map(listener => listener(data));
-        } else {
-            this.callbacks['write'].map(listener => listener(data));
-        }
+        } 
+        // Else, send data to client
+        else this.callbacks['write'].map(listener => listener(data));
     };
+
+    // Close VirtualHost
     this.close = () => this.host.close();
-    this.host = new net.Server(); // create host
+
+    // Create VirtualHost's TCP server for clients to connect
+    this.host = new net.Server();
+
+    // Connection handler for TCP server
     this.host.on('connection', Client => {
-        const id = Math.floor(Math.random() * 900000000000) + 100000000000; // create id which has length of 12
+        // Create an id which has length of 12
+        const id = Math.floor(Math.random() * 900000000000) + 100000000000;
         log(`New client created: Client${id}`, `VirtualHost:${this.port}`);
+
+        // Tell Middler Connector that new client connected
         this.callbacks['data'].map(listener => listener(`${id}NEW_CLIENT`));
-        Client.on('data', data => this.callbacks['data'].map(listener => listener(Buffer.from(ascii_to_hex(id.toString()) + data.toString('hex'), 'hex'))));
+
+        // Forward data sent by client to Middler Connector
+        Client.on('data', data => this.callbacks['data'].map(listener => listener(Buffer.from(ascii_to_hex(id.toString()) + data.toString('hex'), 'hex')))); 
+        
+        // Log if error occurs
         Client.on('error', (err) => log(err, `Client${id}_Host:${this.port}`));
+
+        // If client is closed, let Middler Connector know
         Client.on('close', () => {
             this.callbacks['data'].map(listener => listener(`${id}CLIENT_LOST`));
             log('Client closed.', `Client${id}_Host:${this.port}`)
         });
+
+        // Destroy client if closed
         this.on('client_close', (data) => Buffer.from(data.toString('hex').substring(0, 24), 'hex') == id ? Client.destroy() : {});
+
+        // Send data to client
         this.on('write', (data) => Buffer.from(data.toString('hex').substring(0, 24), 'hex') == id ? Client.write(Buffer.from(data.toString('hex').substring(24, data.toString('hex').length), 'hex')) : {}); //<<<<<<<<<<<
     });
+
+    // Handle if error occurs on TCP connection
     this.host.on('error', (err) => log(err, `VirtualHost:${this.port}`));
+
+    // Close VirtualHost's TCP server
     this.host.on('close', () => {
         log('Host closed.', `VirtualHost:${this.port}`);
         HostManager.removeHost(this.port);
     });
+
+    // Listen callback functions and add them to callback function storage
     this.on = (event, listener) => {
         if (event == 'data')
             this.callbacks['data'].push(listener);
@@ -74,6 +127,8 @@ const VirtualHost = function () {
         if (event == 'client_close')
             this.callbacks['client_close'].push(listener);
     };
+
+    // Start VirtualHost listening on port
     this.host.listen(this.port, config.ip.tunnel, (err) => {
         if (err) log(`Error: New virtual host CANNOT be created on ${this.port}.`, 'Tunnel');
         log(`New virtual host is running on ${this.port}.`, 'Tunnel');
@@ -84,21 +139,28 @@ const VirtualHost = function () {
     });
 };
 
+// Create tunnel server
 const tunnel = net.createServer(BridgeHost => {
-    const host = new VirtualHost(); // create new simulated host when a new wr2 bridge host connects
-    //BridgeHost.write('111.111.111.111:11111');
+    // Create a new VirtualHost when a Middler Connector connects to Middler Tunnel
+    const host = new VirtualHost();
 
+    // Listen data from client and forward it to Middler Connector
     host.on('data', (data) => {
         BridgeHost.write(data);
         //log(data, 'FromClientToBridge');
     });
+
+    // Listen data from Middler Connector and forward it to client
     BridgeHost.on('data', (data) => {
         host.write(data);
         //log(data, 'FromBridgeToClient');
     });
+
+    // Close VirtualHost if error occurs
     BridgeHost.on('error', () => host.close());
 });
 
+// Start listening on port
 tunnel.listen(config.port.tunnel, config.ip.tunnel, (err) => {
     if (err) throw err;
     log(`Tunnel is running on ${config.port.tunnel}.`);
